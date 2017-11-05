ba1c9465c9b1037c3bd1a260239b5018d3e8b925
Jered Sweeney <sweeneyjered@gmail.com>
2017-11-05 21:10:04 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
-  if array.size == 1
-  array.join(", ")
-elsif array.size == 2
-  array.join(" and ")
+ if array.size == 2 
+   return "#{array[0]} and #{array[1]}"
+ elsif array.size >= 3
+   array[-1].insert(0, "and ")
 end
 end
